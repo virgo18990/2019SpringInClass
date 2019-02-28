0683b0dcdc8c9bc4a3d849803ace10bc36d864aa
Prachi <saxenap1@hawkmail.newpaltz.edu>
2019-02-27 22:07:40 -0500

    Replicated changes to new github account from old git hub account

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express   = require('express');
+const users     = require('./controllers/users');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/users', users);
+
+
+
+app.listen(port, () => console.log(`Example app http://localhost:${port}`));
